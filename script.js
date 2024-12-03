c8081fdef236055a21bb68f3754d8de19ff229ce
WebPraveen01 <47769628+WebPraveen01@users.noreply.github.com>
2024-12-03 23:00:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-//your JS code here. If required.
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').trim();
+}
 
-bands.sort((a, b) => {
-    const bandA = a.replace(/^The /, ''); // Remove "The " from the beginning
-    const bandB = b.replace(/^The /, ''); // Remove "The " from the beginning
-    return bandA.localeCompare(bandB); // Compare the band names
-});
+bands.sort((a, b) => strip(a).localeCompare(strip(b)));
 
 console.log(bands);
