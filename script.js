5cbdf56e7b22cc826ca77fad9106d57b1ff5be47
WebPraveen01 <47769628+WebPraveen01@users.noreply.github.com>
2024-12-04 16:08:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -60,7 +60,11 @@ function customSort(bands) {
   return bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 }
 
-const sortedBands = customSort(bands);
+const sortedBands = customSort(bands)
+
+ sortedBands.forEach(band => {
+  console.log(band);
+});
 
 
 
