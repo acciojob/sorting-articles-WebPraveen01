b19dffaa69530912254a6f436a627fa1b7749100
WebPraveen01 <47769628+WebPraveen01@users.noreply.github.com>
2024-12-04 15:56:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -56,8 +56,8 @@ function strip(bandName) {
   return bandName.replace(/^(a |the |an )/i, '').trim();
 }
 
-function customSort(arr) {
-  return arr.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+function customSort(bands) {
+  return bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 }
 
 const sortedBands = customSort(bands);
