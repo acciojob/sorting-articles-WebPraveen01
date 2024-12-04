014dbe0b852e3134991633cd36d167f2f9a9e308
WebPraveen01 <47769628+WebPraveen01@users.noreply.github.com>
2024-12-04 16:15:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,43 +14,6 @@ const bands = [
   'An Old Dog'
 ];
 
-// // Custom sorting function
-// function customSort(arr) {
-//   return arr.sort((a, b) => {
-//     // If 'Anywhere But Here' should come first
-//     if (a === 'Anywhere But Here') return -1;
-//     if (b === 'Anywhere But Here') return 1;
-
-//     // Sort "The" prefixed items alphabetically
-//     const isTheA = a.startsWith('The ');
-//     const isTheB = b.startsWith('The ');
-
-//     // If both are 'The' prefixed or both are not 'The' prefixed, sort alphabetically
-//     if (isTheA && isTheB) {
-//       return a.localeCompare(b);
-//     }
-//     if (!isTheA && !isTheB) {
-//       return a.localeCompare(b);
-//     }
-
-//     // Non-'The' prefixed items come before 'The' prefixed items
-//     return isTheA ? 1 : -1;
-//   });
-// }
-
-// // Apply custom sort and output each band name on a new line
-// const sortedBands = customSort(bands);
-
-// // Log each band name to the console
-// sortedBands.forEach(band => {
-//   console.log(band);
-// });
-// const ul = document.getElementById('band-list');
-// sortedBands.forEach(band => {
-//   let li = document.createElement('li');
-//   li.textContent = band;
-//   ul.appendChild(li);
-// });
 
 function strip(bandName) {
   return bandName.replace(/^(a |the |an )/i, '').trim();
